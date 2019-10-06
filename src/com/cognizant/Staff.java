1a56d9627e70a3a1dd825dc98adb95ba8c1fd342
Mohammad Javed <ghazanfar9131@gmail.com>
2019-10-06 15:36:00 -0400

    - Added Staff Class

---
+++ src/com/cognizant/Staff.java
@@ -0,0 +1,58 @@
+package com.cognizant;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public abstract class Staff extends Person {
+
+
+    private UUID id;
+    private Date joinDate;
+
+    private Date leaveDate;
+    private List<String> languages;
+
+    public Staff(String firstName, String lastName, Date DOB, char gender) {
+        super(firstName, lastName, DOB, gender);
+        languages = new ArrayList<>();
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    public void addLanguage(String language) {
+        languages.add(language);
+    }
+
+    public Date getLeaveDate() {
+        return leaveDate;
+    }
+
+    public void setLeaveDate(Date leaveDate) {
+        this.leaveDate = leaveDate;
+    }
+}
